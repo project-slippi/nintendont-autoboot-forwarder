0037d1fd5130b408a407ef264ca639a643887ac1
Nikhil Narayana <nikhil.narayana@live.com>
2021-06-03 00:12:59 -0700

    search for boot.dol in every possible spot

--- source/main.c
+++ source/main.c
@@ -11,10 +11,6 @@
 #include <unistd.h>
 #include <ogc/lwp_threads.h>
 #include <sdcard/wiisd_io.h>
-#include "CommonConfig.h"
-
-//comment this out for non-autoboot version
-// #define FW_AUTOBOOT 1
 
 static u8 *EXECUTE_ADDR = (u8*)0x92000000;
 static u8 *BOOTER_ADDR = (u8*)0x92F00000;
@@ -49,8 +45,18 @@ int main(int argc, char *argv[])
 	__io_wiisd.isInserted();
 	fatMount("sd", &__io_wiisd, 0, 4, 64);
 
-	const char *fPath = "sd:/apps/nintendont slippi/boot.dol";
+	const char *fPath = "sd:/apps/Slippi Nintendont/boot.dol";
 	FILE *f = fopen(fPath,"rb");
+	if(!f)
+	{
+		fPath = "sd:/apps/slippi nintendont/boot.dol";
+		f = fopen(fPath,"rb");
+	}
+	if(!f)
+	{
+		fPath = "sd:/apps/nintendont slippi/boot.dol";
+		f = fopen(fPath,"rb");
+	}
 	if(!f)
 	{
 		fPath = "sd:/apps/Nintendont Slippi/boot.dol";
@@ -73,42 +79,6 @@ int main(int argc, char *argv[])
 	DCFlushRange(BOOTER_ADDR,app_booter_bin_size);
 	ICInvalidateRange(BOOTER_ADDR,app_booter_bin_size);
 
-#if FW_AUTOBOOT
-	f = fopen("sd:/slippi_nincfg.bin","rb");
-	if(!f)
-	{
-		printf("slippi_nincfg.bin not found!\n");
-		sleep(2);
-		return -2;
-	}
-	NIN_CFG nincfg;
-	fread(&nincfg,1,sizeof(NIN_CFG),f);
-	fclose(f);
-
-	memset(nincfg.GamePath,0,255);
-	//this config can be modified with whatever settings you want for this game
-	//by default it enables autoboot and sets it to boot from sd (wii vc default)
-	nincfg.Config |= (NIN_CFG_AUTO_BOOT);
-	//for example this line would disable any widescreen bits set in the config
-	//nincfg.Config &= ~(NIN_CFG_USB|NIN_CFG_WIIU_WIDE|NIN_CFG_FORCE_WIDE);
-	strcpy(nincfg.GamePath,"di");
-
-	char *CMD_ADDR = (char*)ARGS_ADDR + sizeof(struct __argv);
-	size_t full_fPath_len = strlen(fPath)+1;
-	size_t full_nincfg_len = sizeof(NIN_CFG)+1;
-	size_t full_args_len = sizeof(struct __argv)+full_fPath_len+full_nincfg_len;
-
-	memset(ARGS_ADDR, 0, full_args_len);
-	ARGS_ADDR->argvMagic = ARGV_MAGIC;
-	ARGS_ADDR->commandLine = CMD_ADDR;
-	ARGS_ADDR->length = full_fPath_len+full_nincfg_len;
-	ARGS_ADDR->argc = 2;
-
-	memcpy(CMD_ADDR, fPath, full_fPath_len);
-	memcpy(CMD_ADDR+full_fPath_len, &nincfg, sizeof(NIN_CFG));
-	CMD_ADDR[full_fPath_len+sizeof(NIN_CFG)] = 0;
-	DCFlushRange(ARGS_ADDR, full_args_len);
-#else
 	char *CMD_ADDR = (char*)ARGS_ADDR + sizeof(struct __argv);
 	size_t full_fPath_len = strlen(fPath)+1;
 	size_t full_args_len = sizeof(struct __argv)+full_fPath_len;
@@ -121,7 +91,6 @@ int main(int argc, char *argv[])
 
 	memcpy(CMD_ADDR, fPath, full_fPath_len);
 	DCFlushRange(ARGS_ADDR, full_args_len);
-#endif
 
 	//possibly affects nintendont speed?
 	fatUnmount("sd:");
